d7d7ab3d706801c85d251f7557075d843cdc3455
Eduardo Koloma Jr <5549816+olpie101@users.noreply.github.com>
2024-04-27 00:57:11 +0200

    :sparkles: configure neovim (#1)
    * :see_no_evil: add gitignore
    
    * :sparkles: add initial nvim config
    
    * :lipstick: update alacritty theme to tokyonight

---
+++ .config/nvim/lua/lazy-plugins.lua
@@ -0,0 +1,35 @@
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins, you can run
+--    :Lazy update
+--
+-- NOTE: Here is where you install your plugins.
+require('lazy').setup {
+  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  -- NOTE: Plugins can also be added by using a table,
+  -- with the first argument being the link and the following
+  -- keys can be used to configure plugin behavior/loading/etc.
+  --
+  -- Use `opts = {}` to force a plugin to be loaded.
+  --
+  --  This is equivalent to:
+  --    require('Comment').setup({})
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  require 'kickstart.plugins.indent_line',
+  -- require 'kickstart.health',
+
+  { import = 'custom.plugins' },
+}
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
