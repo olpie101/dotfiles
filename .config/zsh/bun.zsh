b0c7451604dc2149f580907f84cf588b7f75b940
Eduardo Koloma Junior <eduardo@bridgelabs.design>
2026-02-17 16:41:33 +0100

    fix: make bun config portable using $HOME
    - Replace hardcoded path with $BUN_INSTALL variable
    - Add bun.zsh to sourced modules

--- .config/zsh/bun.zsh
+++ .config/zsh/bun.zsh
@@ -1,7 +1,7 @@
-# bun completions
-[ -s "/Users/eduardokolomajr/.bun/_bun" ] && source "/Users/eduardokolomajr/.bun/_bun"
-
 # bun
 export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
+[[ -d "$BUN_INSTALL/bin" ]] && export PATH="$BUN_INSTALL/bin:$PATH"
+
+# bun completions
+[[ -s "$BUN_INSTALL/_bun" ]] && source "$BUN_INSTALL/_bun"
 
