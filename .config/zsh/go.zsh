982f59e2639f43488bb27da768ce61e921ceb437
Eduardo Koloma Junior <eduardo@bridgelabs.design>
2026-02-18 12:15:22 +0100

    fix: add Linux Go installation path (/usr/local/go/bin)

--- .config/zsh/go.zsh
+++ .config/zsh/go.zsh
@@ -1,3 +1,8 @@
 export GOPATH="$HOME/go"
 export PATH="$GOPATH/bin:$PATH"
 
+# Go installation path (Linux typically uses /usr/local/go)
+if [[ "$OSTYPE" == "linux"* ]] && [[ -d "/usr/local/go/bin" ]]; then
+  export PATH="$PATH:/usr/local/go/bin"
+fi
+
